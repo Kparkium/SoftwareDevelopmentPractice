ad579d9197abf9c1b7179bfafc028603878cd454
SwagTemplar <kisoonpark.red@gmail.com>
2023-03-18 15:03:55 +1300

    HP Bar UI
    The UI for the HP bar is created and reacts according to the HP of the player.

--- SDPGame/Assets/Scripts/PlayerBehaviour.cs
+++ SDPGame/Assets/Scripts/PlayerBehaviour.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
+    [SerializeField] HealthBar _healthBar; //HB Script. Lets you drag an object with another script onto an object that has this class in it.
+
+
     void Start()
     {
         
@@ -25,9 +29,16 @@ public class PlayerBehaviour : MonoBehaviour
     }
 
     //Player takes damage
-    private void PlayerTakeDmg(int dmg) => GameManager.gameManager._playerHealth.DamageUnit(dmg);
-
+    private void PlayerTakeDmg(int dmg)
+    {
+        GameManager.gameManager._playerHealth.DamageUnit(dmg);
+        _healthBar.SetHealth(GameManager.gameManager._playerHealth.Health); //Changes the UI according to how much HP you have.
+    }
 
     //Player receives healing
-    private void PlayerRecovery(int heal) => GameManager.gameManager._playerHealth.HealUnit(heal);
+    private void PlayerRecovery(int heal)
+    {
+        GameManager.gameManager._playerHealth.HealUnit(heal);
+        _healthBar.SetHealth(GameManager.gameManager._playerHealth.Health); //Changes the UI according to how much HP you have.
+    }
 }
